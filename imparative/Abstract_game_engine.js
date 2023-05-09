68353332abd79ce21d575970ba0090570b604cf3
nagui-mostafa <nagui.mostafa@gmail.com>
2023-05-09 20:11:34 +0300

    while loop added

--- imparative/Abstract_game_engine.js
+++ imparative/Abstract_game_engine.js
@@ -1,38 +1,35 @@
 export class  Abstract_game_engine {
-
     constructor() {
         if (new.target === Abstract_game_engine) {
             throw new TypeError("Cannot construct Abstract instances directly");
         }
-
-
-
     }
 
-     async Initialize(theGame) {
-         let board = this.createBoard() //intial board
-         console.log(board)
-         let currentPlayer = 1
-         let state={board,currentPlayer}
-         state.board=board
-         state.currentPlayer=currentPlayer
-         this.Drawer(state)
-         await this.sleep(1000)
+      async Initialize(theGame) {
+          let board = this.createBoard() //intial board
+          console.log(board)
+          let currentPlayer = 1
+          let state = {board, currentPlayer}
+          state.board = board
+          state.currentPlayer = currentPlayer
+          this.Drawer(state)
 
-         const loop = async () => {
-             await this.sleep(1000)
-             let new_state = this.Controller(state, this.takeUserInput(theGame));
-             console.table(state)
-             if (new_state != null) {
-                 this.Drawer(new_state)
-                 state=new_state
-             } else {
-                 alert("Invalid Move")
-             }
-             loop(); // Call the function again to repeat the loop
-         }
-         loop(); // Call the function to start the loop
-     }
+          while (true) {
+              await this.sleep(1000)
+              let input = this.takeUserInput()
+              console.log(input)
+              let new_state = this.Controller(state, input);
+              console.table(state)
+              if (new_state != null) {
+                  this.Drawer(new_state)
+                  state = new_state
+              } else {
+                  alert("Invalid Move")
+              }
+
+          }
+
+      }
 
 
     // Used in Constructor to create the game initial board (2D array) based on its type
@@ -43,34 +40,17 @@ export class  Abstract_game_engine {
         throw new Error("Drawer method must be implemented");
         
     }
-    // takeUserInput1() {
-    //     let input1 = document.getElementById("firstInput").value;
-    //     this.Controller(input1);
-    // }
-    // takeUserInput2() {
-    //     let input1 = document.getElementById("firstInput").value;
-    //     let input2 = document.getElementById("secondInput").value;
-    //     this.Controller(input1,input2);
-    // }
-    takeUserInput()
-    {
-        let input= prompt(this.InputMessage())
-        return input
-
+    Controller(state,Input) {
+        throw new Error("Controller method must be implemented");
     }
     InputMessage(){
         throw new Error("InputMessage method must be implemented");
     }
-    Controller(state,Input) {
-        throw new Error("Controller method must be implemented");
-    }
+    takeUserInput() {
 
-    SwitchPlayers(state){
-        state.currentPlayer=!state.currentPlayer;
-    }
+        let input = prompt(this.InputMessage())
+        return input
 
-    ClearInput(id){
-        document.getElementById(id).value='';
     }
 
     FindRowCol(Input){
@@ -82,34 +62,15 @@ export class  Abstract_game_engine {
     isValidLength(Input,expectedLength){
         return Input.length === expectedLength;
     }
-    // isValidLength(Input1,expectedLength1,Input2,expectedLength2){
-    //     return (Input1.length === expectedLength1 )&&(Input2.length === expectedLength2);
-    // }
-
     sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-
     isCellInBounds(board,row, col) {
         return row >= 0 && row < board.length && col >= 0 && col < board[0].length;
     }
-    drawFisrtRow(style){
-        for (let i = 0 ;i< state[0].length; i++){
-            const cell = document.createElement("td");
-            cell.style=style//'height:60px;width:60px;margin:1px;vertical-align: middle;text-align:center;font-size: 25px;box-shadow: #000;background-color:#e0e0d8';
-            let ascii=i+97
-            ascii='&#0'+ascii
-            cell.innerHTML=ascii
-            row.appendChild(cell);
-        }
+    SwitchPlayers(state){
+        state.currentPlayer=!state.currentPlayer;
     }
-
-    // isCellInBounds(row1, col1,row2,col2) {
-    //     return (row1 >= 0 && row1 < this.rowNumbers && col1 >= 0 && col1 < this.colNumbers)&&(row2 >= 0 && row2 < this.rowNumbers && col2 >= 0 && col2 < this.colNumbers);
-    // }
-
-
-
 
 
 }
