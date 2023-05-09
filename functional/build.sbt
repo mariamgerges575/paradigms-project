66839ed5dcd6fde65c757183383c6fc516739cb7
mariamelsamni <96484561+mariamelsamni@users.noreply.github.com>
2023-05-09 21:35:57 +0300

    jarrrr

--- functional/build.sbt
+++ functional/build.sbt
@@ -1,3 +1,5 @@
+import sbt.TestResultLogger.Defaults.Main
+
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.2.2"
--- functional/src/main/scala/Main.scala
+++ functional/src/main/scala/Main.scala
@@ -2,35 +2,35 @@ import java.awt.event.ActionEvent
 import javax.swing.{AbstractAction, JButton, JFrame, JOptionPane, JPanel, JTextField}
 import scala.io.StdIn
 
-object Main extends App {
+    object Main extends App {
 
-    def initialScreen(): Unit = {
-        println("CHOOSE GAME :")
-        println("1.Chess")
-        println("2.Checkers")
-        println("3.Sudoku")
-        println("4.Eight Queens")
-        println("5.Connect 4")
-        println("6.Tic Tac Toe")
-        println("7.Exit")
-        var game = StdIn.readLine()
-        var Exit = false
-        while (!Exit) {
-            game match {
-                case "1" => gameEngine(chessDrawer,initChessBoard,chessController)
-                case "2" => gameEngine(checkersDrawer,initCheckerState,checkersController)
-                case "3" => gameEngine(sudokuDrawer,initSudokuState,sudokuController)
-                case "4" => gameEngine(eightQueensDrawer,init8QsBoard,eightQueensController)
-                case "5" => gameEngine(connect4Drawer,initConnect4Board,connect4Controller)
-                case "6" => gameEngine(ticTacToeDrawer, initTTTBoard, tictactoeController)
-                case "7" => sys.exit()
-                case _ => println("Invalid Input")
+        def initialScreen(): Unit = {
+            println("CHOOSE GAME :")
+            println("1.Chess")
+            println("2.Checkers")
+            println("3.Sudoku")
+            println("4.Eight Queens")
+            println("5.Connect 4")
+            println("6.Tic Tac Toe")
+            println("7.Exit")
+            var game = StdIn.readLine()
+            var Exit = false
+            while (!Exit) {
+                game match {
+                    case "1" => gameEngine(chessDrawer, initChessBoard, chessController)
+                    case "2" => gameEngine(checkersDrawer, initCheckerState, checkersController)
+                    case "3" => gameEngine(sudokuDrawer, initSudokuState, sudokuController)
+                    case "4" => gameEngine(eightQueensDrawer, init8QsBoard, eightQueensController)
+                    case "5" => gameEngine(connect4Drawer, initConnect4Board, connect4Controller)
+                    case "6" => gameEngine(ticTacToeDrawer, initTTTBoard, tictactoeController)
+                    case "7" => sys.exit()
+                    case _ => println("Invalid Input")
+                }
+                game = StdIn.readLine("CHOOSE GAME :")
             }
-            game = StdIn.readLine("CHOOSE GAME :")
         }
+
+        initialScreen()
+
+
     }
-    initialScreen()
-
-
-
-}
